494de30cae4fcc8450a88788cd945d24fdeacd38
michael <parazjx@gmail.com>
2020-06-15 11:31:43 +0800

    [new]: deploy to server

--- build.sh
+++ build.sh
@@ -8,3 +8,9 @@ cp -r dep dist
 cp -r font dist
 cp empty.html proxy.html dist
 echo 'build to `dist` done'
+
+scp -r dist/*  bigfish@192.168.1.126:/home/bigfish/sites/fontAdjust
+
+echo 'deploy to server success:'
+date
+
